ca6b3dddd566600a5ded5e60901cd21672232dd7
Taylor Finklea <taylor.finklea@gmail.com>
2024-12-31 16:47:21 -0500

    update config to match sweep

--- keyboards/keychron/k8_pro/ansi/rgb/keymaps/k8/keymap.c
+++ keyboards/keychron/k8_pro/ansi/rgb/keymaps/k8/keymap.c
@@ -27,6 +27,7 @@ enum layers{
   L6,
   L7,
   L8,
+  L9,
   WIN_BASE,
   WIN_FN
 };
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_NO,                       KC_Q,              MT(MOD_LCTL,KC_W), MT(MOD_LALT,KC_F), KC_P,       KC_B,           KC_NO,    KC_J,     KC_L,       MT(MOD_RALT,KC_U),   MT(MOD_RCTL,KC_Y),   KC_QUOT,                       KC_NO,     KC_NO,   KC_DEL,    KC_END,   KC_PGDN,
      KC_NO,                       MT(MOD_LSFT,KC_A), LT(4,KC_R),        LT(3,KC_S),        LT(2,KC_T), KC_G,           KC_NO,    KC_M,     LT(5,KC_N), LT(6,KC_E),          LT(7,KC_I),          MT(MOD_RSFT,KC_O), KC_ENT,
                                   MT(MOD_HYPR,KC_Z), KC_X,              KC_C,              KC_D,       KC_V,    KC_NO, KC_NO,    KC_K,     KC_H,       KC_COMM,             KC_DOT,              LT(1,KC_SCLN),                                                KC_UP,
-     KC_NO,    KC_NO,             MT(MOD_LGUI,KC_TAB),                                                                 LT(8,KC_SPC),                                        MT(MOD_RGUI,KC_ENT),                    KC_NO, MO(L1), KC_NO,           KC_LEFT,   KC_DOWN,  KC_RGHT
+     KC_NO,    KC_NO,             MT(MOD_LGUI,KC_TAB),                                                                 LT(9,KC_SPC),                                        KC_BSPC,             MT(MOD_RGUI,KC_ENT),       MO(L1), KC_NO,          KC_LEFT,   KC_DOWN,  KC_RGHT
 ),
 
 [L1] = LAYOUT_tkl_ansi(
@@ -48,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      KC_TRNS,                     RGB_MOD,           RGB_VAI,           RGB_HUI,           RGB_SAI,    RGB_SPI,        KC_NO,  KC_TRNS,  KC_TRNS,    KC_TRNS,             KC_TRNS,             KC_TRNS,           KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,  KC_TRNS,
      KC_TRNS,                     RGB_RMOD,          RGB_VAD,           RGB_HUD,           RGB_SAD,    RGB_SPD,        KC_NO,  KC_TRNS,  KC_TRNS,    KC_TRNS,             KC_TRNS,             KC_TRNS,           KC_TRNS,
-                                  QK_MAKE,           QK_BOOT,           DB_TOGG,           EE_CLR,     BAT_LVL, KC_NO, KC_NO,  KC_TRNS,  KC_TRNS,    KC_TRNS,             KC_TRNS,             KC_TRNS,                                                       KC_TRNS,
-     KC_TRNS,  KC_TRNS,           QK_RBT,                                                                              KC_TRNS,                                           KC_TRNS,                                KC_TRNS, KC_TRNS,  KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS
+                                  QK_MAKE,            KC_NO,             DB_TOGG,           EE_CLR,     BAT_LVL, KC_NO, KC_NO,  EE_CLR,   DB_TOGG,    KC_TRNS,             KC_TRNS,             KC_TRNS,                                                       KC_TRNS,
+     KC_TRNS,  KC_TRNS,           QK_BOOT,                                                                             QK_BOOT,                                           QK_BOOT,             QK_MAKE,                    KC_TRNS,  KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
 [L2] = LAYOUT_tkl_ansi(
@@ -57,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            KC_TRNS,             KC_TRNS,           KC_TRNS,   KC_TRNS,                                 KC_TRNS,   KC_TRNS,  KC_TRNS,
 
      KC_TRNS,                     KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_NO,      KC_NO,          KC_NO,  KC_TRNS,  KC_PGUP,   KC_UP,              KC_HOME,             KC_TRNS,           KC_TRNS,   KC_TRNS,              KC_DEL,    KC_END,   KC_PGDN,
-     KC_TRNS,                     KC_ESC,            KC_BSPC,           KC_DEL,            KC_NO,      KC_NO,          KC_NO,  KC_TRNS,  KC_LEFT,   KC_DOWN,            KC_RGHT,             KC_TRNS,           KC_TRNS,
+     KC_TRNS,                     KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_NO,      KC_NO,          KC_NO,  CW_TOGG,  KC_LEFT,   KC_DOWN,            KC_RGHT,             KC_TRNS,           KC_TRNS,
                                   KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_NO,   KC_NO, KC_NO,  KC_TRNS,  KC_PGDN,   G(S(KC_X)),         KC_END,              KC_TRNS,                                                       KC_UP,
-     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_ENT,                                          KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_ESC,                                          KC_DEL,              KC_TRNS,                    KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
 [L3] = LAYOUT_tkl_ansi(
@@ -69,17 +70,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,                     KC_TRNS,           KC_TRNS,           KC_NO,             KC_TRNS,    KC_TRNS,        KC_NO,  KC_PSLS,  KC_P7,     KC_P8,              KC_P9,               KC_PMNS,           KC_TRNS,   KC_TRNS,              KC_DEL,    KC_END,   KC_PGDN,
      KC_TRNS,                     KC_TRNS,           KC_PCMM,           KC_NO,             KC_PDOT,    KC_TRNS,        KC_NO,  KC_PAST,  KC_P4,     KC_P5,              KC_P6,               KC_PPLS,           KC_TRNS,
                                   KC_TRNS,           KC_TRNS,           KC_NO,             KC_TRNS,    KC_TRNS, KC_NO, KC_NO,  KC_TRNS,  KC_P1,     KC_P2,              KC_P3,               KC_PEQL,                                                       KC_UP,
-     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_ENT,                                          KC_P0,                                KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TRNS,                                         KC_TRNS,             KC_P0,                      KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
 [L4] = LAYOUT_tkl_ansi(
      KC_TRNS,  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            KC_TRNS,             KC_TRNS,           KC_TRNS,                                            KC_TRNS,   KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            KC_TRNS,             KC_TRNS,           KC_TRNS,   KC_TRNS,                                 KC_TRNS,   KC_TRNS,  KC_TRNS,
 
-     KC_TRNS,                     KC_TRNS,           KC_NO,             G(KC_F),           KC_TRNS,    KC_TRNS,        KC_NO,    KC_TRNS,  KC_VOLD,   KC_MUTE,            KC_VOLU,             KC_TRNS,           KC_TRNS,   KC_TRNS,              KC_DEL,    KC_END,   KC_PGDN,
+     KC_TRNS,                     KC_TRNS,           KC_NO,             G(KC_F),           G(KC_A),    KC_TRNS,        KC_NO,    KC_TRNS,  KC_VOLD,   KC_MUTE,            KC_VOLU,             KC_TRNS,           KC_TRNS,   KC_TRNS,              KC_DEL,    KC_END,   KC_PGDN,
      KC_TRNS,                     G(KC_K),           KC_NO,             G(KC_V),           G(KC_C),    G(KC_X),        KC_NO,    KC_MCTL,  KC_MRWD,   KC_MPLY,            KC_MFFD,             KC_SIRI,           KC_TRNS,
                                   KC_TRNS,           KC_NO,             G(KC_Z),           KC_TRNS,    KC_TRNS, KC_NO, KC_NO,    KC_TRNS,  KC_BRIU,   KC_BRID,            KC_LPAD,             KC_TRNS,                                                       KC_UP,
-     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TRNS,                                           KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TRNS,                                           KC_TRNS,             KC_TRNS,                    KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
 [L5] = LAYOUT_tkl_ansi(
@@ -89,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TRNS,                     KC_GRV,            KC_PIPE,           KC_LBRC,           KC_RBRC,    KC_DLR,         KC_NO,    KC_NO,    KC_NO,     KC_ASTR,            KC_UNDS,             KC_PLUS,           KC_TRNS,   KC_TRNS,              KC_DEL,    KC_END,   KC_PGDN,
      KC_TRNS,                     KC_HASH,           KC_SLSH,           KC_LPRN,           KC_RPRN,    KC_PERC,        KC_NO,    KC_NO,    KC_NO,     KC_AT,              KC_MINS,             KC_COLN,           KC_TRNS,
                                   KC_CIRC,           KC_BSLS,           KC_LCBR,           KC_RCBR,    KC_AMPR, KC_NO, KC_NO,    KC_NO,    KC_NO,     KC_EXLM,            KC_QUES,             KC_SCLN,                                                       KC_UP,
-     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TRNS,                                           KC_TRNS,                                KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TILD,                                           KC_PEQL,             KC_TRNS,                    KC_TRNS, KC_TRNS,       KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
 [L6] = LAYOUT_tkl_ansi(
@@ -108,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      KC_TRNS,                     KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,        KC_NO,  KC_TRNS,    DF(L3),        KC_TRNS,        KC_NO,             KC_NO,             KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,  KC_TRNS,
      KC_TRNS,                     KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,        KC_NO,  DF(L2),     DF(MAC_BASE),  KC_TRNS,        KC_NO,             KC_TRNS,           KC_TRNS,
-                                  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS, KC_NO, KC_NO,  KC_TRNS,    DF(L8),        KC_TRNS,        KC_NO,             KC_NO,                                                       KC_TRNS,
+                                  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS, KC_NO, KC_NO,  DF(L9),     DF(L8),        KC_TRNS,        KC_NO,             KC_NO,                                                       KC_TRNS,
      KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_TRNS,                                           KC_TRNS,                                KC_TRNS, KC_TRNS,  KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
@@ -118,10 +119,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      KC_TRNS,                     KC_NO,             KC_WH_L,           KC_WH_U,           KC_WH_R,    KC_NO,          KC_NO,  KC_NO,      KC_NO,         KC_MS_U,        KC_NO,             KC_NO,             KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,  KC_TRNS,
      KC_TRNS,                     KC_BTN4,           KC_BTN2,           KC_WH_D,           KC_BTN1,    KC_BTN5,        KC_NO,  KC_NO,      KC_MS_L,       KC_MS_D,        KC_MS_R,           KC_NO,             KC_TRNS,
-                                  KC_NO,             KC_NO,             KC_BTN3,           KC_NO,      KC_NO,   KC_NO, KC_NO,  KC_NO,      KC_NO,         KC_NO,          KC_NO,             KC_NO,                                                       KC_TRNS,
+                                  KC_NO,             KC_NO,             KC_BTN3,           KC_NO,      KC_NO,   KC_NO, KC_NO,  KC_NO,      KC_NO,         KC_NO,          KC_NO,             DF(MAC_BASE),                                                       KC_TRNS,
      KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_NO,                                             KC_TRNS,                                KC_TRNS, KC_TRNS,  KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS
 ),
 
+[L9] = LAYOUT_tkl_ansi(
+     KC_TRNS,  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            KC_TRNS,             KC_TRNS,           KC_TRNS,                                            KC_TRNS,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,           KC_TRNS,           KC_TRNS,           KC_TRNS,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            KC_TRNS,             KC_TRNS,           KC_TRNS,   KC_TRNS,                                 KC_TRNS,   KC_TRNS,  KC_TRNS,
+
+     KC_TRNS,                     KC_NO,             KC_WH_L,           KC_WH_U,           KC_WH_R,    KC_NO,          KC_NO,  KC_NO,      KC_WH_L,       KC_WH_U,        KC_WH_R,           KC_NO,             KC_TRNS,   KC_TRNS,              KC_TRNS,   KC_TRNS,  KC_TRNS,
+     KC_TRNS,                     KC_BTN4,           KC_BTN2,           KC_WH_D,           KC_BTN1,    KC_BTN5,        KC_NO,  KC_BTN4,    KC_BTN1,       KC_WH_D,        KC_BTN2,           KC_BTN5,             KC_TRNS,
+                                  KC_NO,             KC_NO,             KC_BTN3,           KC_NO,      KC_NO,   KC_NO, KC_NO,  KC_NO,      KC_NO,         KC_BTN3,        KC_NO,             DF(MAC_BASE),                                                       KC_TRNS,
+     KC_TRNS,  KC_TRNS,           KC_TRNS,                                                                             KC_NO,                                             KC_TRNS,                                KC_TRNS, KC_TRNS,  KC_TRNS,      KC_TRNS,   KC_TRNS,  KC_TRNS
+),
 
 [WIN_BASE] = LAYOUT_tkl_ansi(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,   KC_CTANA, RGB_MOD,
